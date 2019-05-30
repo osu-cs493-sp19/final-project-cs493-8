9ed7bb2bf4f22b3be69cd80c7b8b8dfc23f70a87
jmanosu <jmanosu@gmail.com>
2019-05-29 23:37:22 -0700

    inital files

---
+++ models/user.js
@@ -0,0 +1,88 @@
+/*
+ * User schema and data accessor methods;
+ */
+const bcrypt = require('bcryptjs');
+
+const mysqlPool = require('../lib/mysqlPool');
+const { extractValidFields } = require('../lib/validation');
+const { getReviewsByBusinessId } = require('./review');
+const { getPhotosByBusinessId } = require('./photo');
+
+
+/*
+ * Schema describing required/optional fields of a business object.
+ */
+const UserSchema = {
+  name: { required: true },
+  email: { required: true },
+  password: { required: true },
+  admin: { required: false }
+};
+
+exports.UserSchema = UserSchema;
+
+insertNewUser = async (user) =>{
+    return new Promise(async (resolve, reject) => {
+        user = extractValidFields(user, UserSchema);
+        user.password = await bcrypt.hash(user.password, 8);
+        user.id = null;
+        mysqlPool.query(
+            'INSERT INTO users SET ?',
+            user,
+            (err, result) => {
+                if (err) {
+                reject(err);
+                } else {
+                resolve(result.insertId);
+                }
+            }
+        );
+    });
+}
+
+exports.insertNewUser = insertNewUser;
+
+getUserByEmail = async (email, includePassword) => {
+    return new Promise((resolve, reject) => {
+        mysqlPool.query(
+            'SELECT' + (includePassword ? ' * ' : ' id, name, email, admin ') + 'FROM users WHERE email = ?',
+            [ email ],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
+}
+
+exports.getUserByEmail = getUserByEmail;
+
+getUserById = async (id, includePassword) => {
+    return new Promise((resolve, reject) => {
+        mysqlPool.query(
+            'SELECT' + (includePassword ? ' * ' : ' id, name, email, admin ') + 'FROM users WHERE id = ?',
+            [ id ],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
+}
+
+exports.getUserById = getUserById;
+
+validateUser = async (email, password) => {
+    let user = (await getUserByEmail(email, true))[0];
+    const authenticated = user && await bcrypt.compare(password, user.password);
+    return authenticated;
+}
+
+exports.validateUser = validateUser;
+
